44a1c3a889e1c421e9cea25c5188c1b29f2728e8
Avilolo <avishaydadon17@gmail.com>
2022-12-17 20:17:52 +0200

    final commit

--- app/src/main/java/com/example/hw1/GameActivity.java
+++ app/src/main/java/com/example/hw1/GameActivity.java
@@ -1,19 +1,25 @@
 package com.example.hw1;
 
+import android.content.Context;
 import android.os.Bundle;
-
+import android.os.Vibrator;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.imageview.ShapeableImageView;
+
 
 public class GameActivity extends AppCompatActivity {
 
-    FloatingActionButton rightButton;
-    FloatingActionButton leftButton;
-    ShapeableImageView hearts[];
-    ShapeableImageView frogs[];
-    ShapeableImageView crocs[][];
+    private ExtendedFloatingActionButton rightButton;
+    private ExtendedFloatingActionButton leftButton;
+    private ShapeableImageView hearts[];
+    private ShapeableImageView frogs[];
+    private ShapeableImageView crocs[][];
+    private Vibrator vibrator;
+    private static int frogPos = 1;
+
+    GameData gameData;
 
 
     @Override
@@ -22,8 +28,18 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViews();
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        gameData = new GameData(hearts.length, vibrator);
 
-        rightButton.setOnClickListener(view -> {});
+        rightButton.setOnClickListener(view -> {
+            rightClick();
+        });
+        leftButton.setOnClickListener(view -> {
+            leftClick();
+        });
+
+        gameData.makeCrocBySeconds(crocs, frogs, frogPos);
+        gameData.moveCrocBySeconds(crocs, frogs, hearts, this.getApplicationContext());
     }
 
     public void findViews() {
@@ -40,22 +56,42 @@ public class GameActivity extends AppCompatActivity {
                 findViewById(R.id.game_frog_52)
         };
         crocs = new ShapeableImageView[][]{
-                { findViewById(R.id.game_croc_10),
-                  findViewById(R.id.game_croc_20),
-                  findViewById(R.id.game_croc_30),
-                  findViewById(R.id.game_croc_40) },
-                { findViewById(R.id.game_croc_11),
-                  findViewById(R.id.game_croc_21),
-                  findViewById(R.id.game_croc_31),
-                  findViewById(R.id.game_croc_41) },
-                { findViewById(R.id.game_croc_12),
-                  findViewById(R.id.game_croc_22),
-                  findViewById(R.id.game_croc_32),
-                  findViewById(R.id.game_croc_42) }};
+                {findViewById(R.id.game_croc_10),
+                        findViewById(R.id.game_croc_20),
+                        findViewById(R.id.game_croc_30),
+                        findViewById(R.id.game_croc_40)},
+                {findViewById(R.id.game_croc_11),
+                        findViewById(R.id.game_croc_21),
+                        findViewById(R.id.game_croc_31),
+                        findViewById(R.id.game_croc_41)},
+                {findViewById(R.id.game_croc_12),
+                        findViewById(R.id.game_croc_22),
+                        findViewById(R.id.game_croc_32),
+                        findViewById(R.id.game_croc_42)}};
     }
 
-    public void onClick(){
+    public void rightClick() {
+        if (frogPos != 2) {
+            frogs[frogPos].setVisibility(View.INVISIBLE);
+            frogPos++;
+            frogs[frogPos].setVisibility(View.VISIBLE);
+        }
+    }
 
+    public void leftClick() {
+        if (frogPos != 0) {
+            frogs[frogPos].setVisibility(View.INVISIBLE);
+            frogPos--;
+            frogs[frogPos].setVisibility(View.VISIBLE);
+        }
+    }
+
+    public static int getFrogPos() {
+        return frogPos;
     }
 }
+
+
+
+
 
